945851b45292e2c42030f7659c07823aab2a2e33
cmis <cmis@cmis-hs06.(none)>
2015-08-20 14:36:46 +0700

    Favorite

---
+++ Fundemental1/Fundemental1.java
@@ -0,0 +1,16 @@
+package Fundemental1;
+
+public class Fundemental1 
+    {
+     public static void main ( String args[]) 
+        {
+        System.out.println( "DJ's favorite" ); 
+        System.out.println( "Food = Pad Thai" ); 
+        System.out.println( "Colour = Blue" ); 
+        System.out.println( "Movie = Guardians of the Galaxy" ); 
+        System.out.println( "Music = Jaymes Young" ); 
+           
+        } // end method 
+        
+    } // end class 
+     
