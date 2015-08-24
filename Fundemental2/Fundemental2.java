41f722bccffe16b7513f7bf69894d828c8a65410
cmis <cmis@cmis-hs06.(none)>
2015-08-24 09:52:41 +0700

    Fundenmental2

---
+++ Fundemental2/Fundemental2.java
@@ -0,0 +1,15 @@
+package Fundemental2;
+
+import javax.swing.JOptionPane; 
+
+public class Fundemental2
+    {
+    public static void main( String args[] )
+        {
+       String input1 = JOptionPane.showInputDialog( "Pick a number"); 
+       String input2 = JOptionPane.showInputDialog( "Pick another number"); 
+       
+       int a = Integer
+        } // end method
+        
+    } // end class
