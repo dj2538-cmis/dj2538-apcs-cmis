10f7b0344f84ab13c53cad59191a6f3fee532a78
cmis <cmis@cmis-hs06.(none)>
2015-08-25 11:52:06 +0700

    Fundemental2

--- Fundemental2/Fundemental2.java
+++ Fundemental2/Fundemental2.java
@@ -9,7 +9,23 @@ public class Fundemental2
        String input1 = JOptionPane.showInputDialog( "Pick a number"); 
        String input2 = JOptionPane.showInputDialog( "Pick another number"); 
        
-       int a = Integer
+       float num1 = Integer.parseInt( input1 );
+       float num2 = Integer.parseInt( input2 ); 
+        
+       if( num1 > num2 )
+          {
+          System.out.println( "Your first pick which is " +num1  + " is greater than your second number which is "  +num2 );
+          }
+       if ( num2 > num1 )
+          { 
+          System.out.println( "Your second pick which is " +num2  + " is greater than your first pick "  +num1 );
+          }
+       
+       if( num1 == num2 )
+          {
+          System.out.println( "Your first pick which is " +num1  + " is the same as your second pick "  +num2 );
+          }
+       
         } // end method
         
     } // end class
