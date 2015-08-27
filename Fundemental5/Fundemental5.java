e1da1da468b0319f74c2d322124007afe7075474
cmis <cmis@cmis-hs00.(none)>
2015-08-27 11:52:43 +0700

    Fundemental5

---
+++ Fundemental5/Fundemental5.java
@@ -0,0 +1,18 @@
+package Fundemental5;
+
+public class Fundemental5
+    {
+     public static void main ( String[] args) 
+        {
+        String movies = ("UP, Inside Out, Guardians of the Galaxy"); 
+        String newLine = System.getProperty("line.separator");
+        
+        if(movies.equals("UP, Inside Out, Guardians of the Galaxy"))
+            {
+            System.out.println("UP" + newLine + "Inside Out" + newLine + "Guardians of the Galaxy");
+            }
+        
+        } // end method 
+        
+    } // end class 
+     
