0f544808a5cdb036710d904aeaa62a981b313a63
cmis <cmis@cmis-hs06.(none)>
2015-09-04 11:56:35 +0700

    Test1done

--- Test1/Test1.java
+++ Test1/Test1.java
@@ -8,7 +8,7 @@ public class Test1
      {
        String input1 = JOptionPane.showInputDialog( "Please choose an integer"); 
        
-       float[] randomNumbers = new float[5];
+       float[] randomNumbers = new float[9];
        for(int index = 0; index < randomNumbers.length;index++)
         {
             randomNumbers[index] = (int)(Math.random()*50 + 1);
@@ -16,9 +16,19 @@ public class Test1
         
        for(int index = 0; index < randomNumbers.length;index++)
         {
-           System.out.print(randomNumbers[index]+ " ");
+           System.out.println(" "+index+" : " + randomNumbers[index]);
+           if(randomNumbers[index]%2==0)
+            {
+                System.out.println("E");
+            }
+
+            else
+            {
+                System.out.println("O");
+            }
         }
+       
      } // end method main 
     
    
-    } // end class Inputname
+    } // end class 
