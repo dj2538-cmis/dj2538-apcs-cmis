59c8057bf192b880a704279cbb60380a48c333a6
cmis <cmis@cmis-hs06.(none)>
2015-09-29 11:41:39 +0700

    ClassDesign1

---
+++ ClassDesignP1/Classsong.java
@@ -0,0 +1,29 @@
+package ClassDesignP1;
+
+public class Classsong 
+    {
+        private String Genre;
+        private String Artist;
+        private String Song;
+        
+        public Classsong()
+          {
+           Genre = new String ("Alternative");
+           Artist = new String ("Coldplay");
+           Song = new String ("Paradise"); 
+          } // end constructor Classsong
+          
+        public Classsong(String Genre, String Artist, String Song)
+          {
+           this.Genre = new String( Genre );
+           this.Artist = new String( Artist);
+           this. Song = new String( Song );
+          } // another constructor 
+          
+        public String toString()
+          {
+           String output = new String();
+           output = "Genre is:
+          }
+    }
+    
