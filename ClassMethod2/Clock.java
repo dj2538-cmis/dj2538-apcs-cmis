2645845709d182b8b9462763d89e63d9de9bc0d3
DJ Ketsing <dj_ketsing@DJs-MacBook-Pro.local>
2015-11-22 20:20:07 +0700

    ClassMethod3

---
+++ ClassMethod2/Clock.java
@@ -0,0 +1,59 @@
+package ClassMethod2;
+
+
+public class Clock
+    {
+        private int hour;
+        private int min;
+        private int sec;
+        private int add; 
+        private int minus;
+        private int total;
+        
+        public Clock()
+          {
+          hour = 15; 
+          min = 45;
+          sec = 23;
+          } // end constructor Classsong
+          
+        
+        public void setTimer( int hour, int min, int sec )
+        {
+         this.hour = hour;
+         this.min = min;
+         this.sec = sec; 
+        }
+        
+        public void converDaylightSavingplus( int hour )
+        {
+          this.hour = hour + 1;
+        }
+        
+        public void converDaylightSavingminus( int hour )
+        {
+          this.hour = hour - 1;
+        }
+        
+        public void totalSeconds(int hour, int min, int sec)
+        {
+         this.hour = hour * 3600;
+         this.min = min * 60;
+         this.sec = sec;
+        }
+        
+        public Clock( int hour, int min, int sec)
+          {
+           this.hour = hour;
+           this.min = min;
+           this. sec = sec;
+          } // another constructor 
+          
+        public String toString()
+          {
+           String output = new String();
+           output = "The time is " +  hour + " : " + min + " : " +  sec; 
+           return output; 
+          }
+    }
+    
---
+++ ClassMethod2/ClockDriver.java
@@ -0,0 +1,32 @@
+package ClassMethod2;
+
+public class ClockDriver
+  {
+   public static void main( String[] args )
+     {
+      Clock clock1 = new Clock();
+      System.out.println( clock1 );
+      
+      System.out.println("\n\n");
+      clock1.setTimer(4, 4, 3);
+      System.out.println( clock1);
+      
+      System.out.println("\n\n");
+      System.out.println("Day lgiht savings +1");
+      clock1.converDaylightSavingplus(4); 
+      System.out.println(clock1);
+      
+      System.out.println("\n\n");
+      System.out.println("Day lgiht savings -1");
+      clock1.converDaylightSavingminus(4); 
+      System.out.println(clock1);
+      
+      System.out.println("\n\n");
+      System.out.println("Total seconds");
+      System.out.println( clock1.totalSeconds() );
+     
+      
+      
+     }
+   
+  }
