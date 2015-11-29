6b224b1bd4ec1225476fafbafaa5b1bdf3684f03
DJ Ketsing <dj_ketsing@DJs-MacBook-Pro.local>
2015-11-29 11:33:29 +0700

    ClassMethod3Done

---
+++ ClassMethod3/StudentDriver.java
@@ -0,0 +1,20 @@
+package ClassMethod3;
+import javax.swing.JOptionPane; 
+
+public class StudentDriver
+  {
+   public static void main( String[] args )
+     {
+      Students senior1 = new Students(); 
+      
+      System.out.println(senior1);
+      
+      System.out.println("\n\n");
+      senior1.setGPA(4.0, 4.0, 4.0, 4.0, 4.0);
+      System.out.println(senior1);
+      
+      
+     
+     }
+   
+  }
