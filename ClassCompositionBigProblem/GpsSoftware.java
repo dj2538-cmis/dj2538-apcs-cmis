8ebea5ea1cc0768a0bc403906beb0209995d084a
cmis <cmis@cmis-hs06.(none)>
2016-01-27 13:30:41 +0700

    bigproblem

---
+++ ClassCompositionBigProblem/GpsSoftware.java
@@ -0,0 +1,13 @@
+package ClassCompositionBigProblem;
+import java.util.ArrayList;
+
+public class GpsSoftware
+   {
+    private ArrayList[][] GPS = new ArrayList;  
+    
+    public GpsSoftware()
+       {
+        this.ArrayList[][] GPS = new ArrayList[13][13];
+       } // end zero-arg constructor 
+       
+   } // end class
