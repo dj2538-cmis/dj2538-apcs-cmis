2875a6ece3191b23d6de65f747239911b1c97345
cmis <cmis@cmis-hs06.(none)>
2016-01-29 09:54:57 +0700

    BigProblem

--- ClassCompositionBigProblem/GpsSoftware.java
+++ ClassCompositionBigProblem/GpsSoftware.java
@@ -3,11 +3,27 @@ import java.util.ArrayList;
 
 public class GpsSoftware
    {
-    private ArrayList[][] GPS = new ArrayList;  
+    private ArrayList<Integer> markers = new ArrayList<Integer>(13);  
     
     public GpsSoftware()
        {
-        this.ArrayList[][] GPS = new ArrayList[13][13];
+        for( int i = 0; i < markers.size(); i++ )
+           {
+            markers.add(i);    
+           }
+        
        } // end zero-arg constructor 
+       
+    public String toString()
+       {
+        String output = new String();
+        
+        for( Integer m : markers )
+           {
+            output += m;   
+           }
+           
+        return output; 
+       } // end toString
        
    } // end class
