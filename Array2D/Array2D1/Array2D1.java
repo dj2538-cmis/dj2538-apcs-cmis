51c517721024f64f3dd1055e25975fe03cf68a89
DJ Ketsing <dj_ketsing@DJs-MacBook-Pro.local>
2016-02-17 11:54:52 +0700

    Edit

--- Array2D/Array2D1/Array2D1.java
+++ Array2D/Array2D1/Array2D1.java
@@ -1,21 +1,15 @@
 package Array2D.Array2D1;
-<<<<<<< HEAD
 import javax.swing.JOptionPane;
-=======
->>>>>>> 77ef905eff0701fd6cf8e97585b011acfd04ef14
 
 public class Array2D1
    {
     public static void main( String[] args )
        {
-<<<<<<< HEAD
+
         int row1 = Integer.parseInt( JOptionPane.showInputDialog( "Enter Rows" ) ); 
         int col1 = Integer.parseInt( JOptionPane.showInputDialog( "Enter Columns" ) ); 
         int[][] grid = new int[row1][col1];
-=======
-        int[][] grid = new int[3][4];
->>>>>>> 77ef905eff0701fd6cf8e97585b011acfd04ef14
-        
+
         for( int row = 0; row < grid.length; row++ )
            {
             for( int col = 0; col < grid[0].length; col++ )
@@ -50,9 +44,6 @@ public class Array2D1
            
        } // end void man
        
-<<<<<<< HEAD
-   } // end class 
-=======
+
    } // end class 
 
->>>>>>> 77ef905eff0701fd6cf8e97585b011acfd04ef14
